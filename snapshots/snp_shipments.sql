43138b60ac404f8aa90edd0deef426c4b71f156f
Vasudevan DS <vasudevan.dshanmugam@cognizant.com>
2025-11-14 06:37:38 +0000

    Snapshots & transformin_dev

--- snapshots/snp_shipments.sql
+++ snapshots/snp_shipments.sql
@@ -4,7 +4,7 @@
     (
 
     target_database = 'qwt_dev',
-    target_schema = env_var('DBT_SNAPSHOTS_SCHEMA', 'SNAPSHOTS_DEV'),
+    target_schema =  env_var('DBT_SNAPSHOTS_SCHEMA', 'SNAPSHOTS_DEV') ,
     unique_key = ['orderid', 'lineno'],
     strategy = 'timestamp',
     updated_at = 'shipmentdate'
